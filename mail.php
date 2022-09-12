d63146c7a410cbf128ce93b0fffda18f2d830398
Itischeat <jamp.ivonin@bk.ru>
2022-09-13 00:39:12 +0300

    ffxah

--- mail.php
+++ mail.php
@@ -16,7 +16,7 @@ foreach($arr as $key => $value) {
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-  echo 'сообщение';
+  header("location: index.html");
 } else {
   echo "Error";
 }
